4bd4f01d614945bb8035ba857631e870144cd338
EuLeEr <eermolov@mail.ru>
2019-11-18 20:29:29 +0300

    uwsgi  finished

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ def do_magic():
     now = datetime.datetime.now()
     return "Hello! {0}".format(now)
 
+# WSGI "wiskey" standart for python
+# sudo apt-get install uwsgi-plugin-python3 
+# uwsgi  --plugin python3 --http-socket :9090 --wsgi-file app.py
+def application(env, start_response):
+    start_response('200 OK', [('Content-type:','text-html')])
+    return [do_magic().encode()]
+
 if __name__=="__main__":
     if 'REQUEST_URI' in os.environ:
       print("Content-type: text/html\n\n")
     print(do_magic())
+ 
